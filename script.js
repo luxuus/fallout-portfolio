26870b1372a5b6cbe3d9fe61a382b5e1486a7e4b
tonyn <tony.noel@etu.umontpellier.fr>
2023-01-18 20:03:52 +0100

    css responsive card

--- script.js
+++ script.js
@@ -28,3 +28,10 @@ var pages = document.getElementsByClassName('page');
            }
         }
   })
+
+  const slider = document.getElementById("slider");
+  const root = document.documentElement;
+
+  slider.addEventListener("input", (e)=> {
+    root.style.setProperty("--card-height", e.target.value + "px");
+  })

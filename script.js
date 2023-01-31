2c1011b663428630fcb58fa112b36cf16df1681c
tonyn <tony.noel@etu.umontpellier.fr>
2023-01-31 23:47:00 +0100

    add gif + fix nav

--- script.js
+++ script.js
@@ -31,7 +31,10 @@ var pages = document.getElementsByClassName('page');
 
   const slider = document.getElementById("slider");
   const root = document.documentElement;
-
+  
   slider.addEventListener("input", (e)=> {
     root.style.setProperty("--card-height", e.target.value + "px");
+    document.getElementById("value").innerText = e.target.value + " px";
   })
+
+  

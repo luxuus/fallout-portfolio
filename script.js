96c48b92b91cd32a9545b2e630a73ec8a417606c
tonyn <tony.noel@etu.umontpellier.fr>
2023-02-21 19:23:15 +0100

    work on svg

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-var pages = document.getElementsByClassName('page');
-  for(var i = 0; i < pages.length; i++)
-    {
-      var page = pages[i];
-      if (i % 2 === 0)
-        {
-          page.style.zIndex = (pages.length - i);
-        }
-    }
-
-  document.addEventListener('DOMContentLoaded', function(){
-    for(var i = 0; i < pages.length; i++)
-      {
-        //Or var page = pages[i];
-        pages[i].pageNum = i + 1;
-        pages[i].onclick=function()
-          {
-            if (this.pageNum % 2 === 0)
-              {
-                this.classList.remove('flipped');
-                this.previousElementSibling.classList.remove('flipped');
-              }
-            else
-              {
-                this.classList.add('flipped');
-                this.nextElementSibling.classList.add('flipped');
-              }
-           }
-        }
-  })
-
-  const slider = document.getElementById("slider");
-  const root = document.documentElement;
+const slider = document.getElementById("slider");
+const root = document.documentElement;
   
-  slider.addEventListener("input", (e)=> {
-    root.style.setProperty("--card-height", e.target.value + "px");
-    document.getElementById("value").innerText = e.target.value + " px";
-  })
+slider.addEventListener("input", (e)=> {
+  root.style.setProperty("--card-height", e.target.value + "px");
+  document.getElementById("value").innerText = e.target.value + " px";
+})
 
   
